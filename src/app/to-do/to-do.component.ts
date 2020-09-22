b54f5a392f8d51586c33b25cff31fc7ef2c08f12
tonykobe8 <tonykobe8@gmail.com>
2020-09-22 12:15:05 +0200

    2 files modified

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -9,6 +9,12 @@ export class ToDoComponent implements OnInit {
 
   constructor() { }
   todo_list=[{content:'fghgfggf',status:'hgfhghg',priority:'ytyyt'},{content:'123',status:'2345',priority:'0087'}];
+  delete(todo){
+const index = this.todo_list.indexOf(todo,0);
+if(index > -1){
+  this.todo_list.splice(index,1) ;
+}
+  }
 
   ngOnInit() {
   }
